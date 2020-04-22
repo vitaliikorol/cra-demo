3ab35df44a8f9f424316e64588d4d1da45824cef
Vitalii Korol <vitaliikorol@github.com>
2020-04-22 10:07:21 +0300

    Updates

--- precache-manifest.c772a96b1351b4fdd535bd64bfac968e.js
+++ precache-manifest.c772a96b1351b4fdd535bd64bfac968e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "991b91a3b6ef4b54be593432a50a31ce",
+    "revision": "8874ad6bae3ccf2704c390020495f3a5",
     "url": "./index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.245c5a62.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1e5f59efdf54a9b31312",
-    "url": "./static/js/main.c004eeb9.chunk.js"
+    "revision": "12d2f369008cfa067016",
+    "url": "./static/js/main.debeed74.chunk.js"
   },
   {
     "revision": "1e1bc86170083df7b205",
---
+++ static/js/main.debeed74.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcra-demo"]=this["webpackJsonpcra-demo"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2);var o=function(){return r.a.createElement("div",{className:"App"},r.a.createElement("h1",null,"Vitalii Korol"))};n.n(c).a.render(r.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.debeed74.chunk.js.map
